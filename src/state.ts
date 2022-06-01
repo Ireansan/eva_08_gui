dd5d4fefd059c44d691f9262f2d5f5d2a4e94f4d
Ireansan <kkntp528@icloud.com>
2022-06-01 14:32:57 +0900

    Revert "tmp"
    This reverts commit 4e521f06c9addbd585bc6255d866057069de3615.

--- src/state.ts
+++
@@ -1,9 +0,0 @@
-import {proxy} from 'valtio'
-import { Hands, Results } from '@mediapipe/hands'
-
-class States {
-    handsData = new Hands();
-    ctx = new CanvasRenderingContext2D();
-}
-
-export const state = proxy(new States());
