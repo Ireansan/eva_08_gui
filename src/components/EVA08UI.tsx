33c0acb54204b93a5caf8b542bd43855d460f87c
Ireansan <kkntp528@icloud.com>
2022-06-16 15:33:31 +0900

    remove not use function

--- src/components/EVA08UI.tsx
+++ src/components/EVA08UI.tsx
@@ -11,108 +11,11 @@ type EVA08UIProps = {
     children: React.ReactElement;
 };
 
-function ActionThumb({ children }: EVA08UIProps) {
-    const ref = useRef<Group>(new Group());
-    const { handResults, eva08state, config } = useSnapshot(states);
-    const viewport = useThree((state) => state.viewport);
-    const results = new MediapipeHelper(handResults, viewport);
-
-    const [configControl, set] = useControls(() => ({
-        v20: { value: 0, label: "radian" },
-        v25: { value: 0, label: "rotation" },
-        v21: { value: 0, label: "count" },
-        v22: { value: true, label: "count Flag" },
-        v23: { value: 0, label: "start Euler" },
-        v24: { value: 0, label: "end Euler" },
-    }));
-
-    useFrame((state, delta) => {
-        if (results.checkLength()) {
-            const i = results.searchIndex(config.label);
-
-            if (i !== undefined) {
-                /*
-                 * p5 -- p4
-                 * |  \   |
-                 * |   \  |
-                 * p9 -- p0
-                 */
-                const angle: number =
-                    Math.PI / 2 - results.angleHinge(5, 4, 0, 9)!;
-                set({ v20: angle });
-
-                // check
-                if (eva08state.countFlag) {
-                    set({ v23: eva08state.startEuler }); //
-                    set({ v22: eva08state.countFlag }); //
-
-                    ref.current.rotation.y = eva08state.startEuler.y + angle!;
-                    set({ v25: ref.current.rotation.y }); //
-
-                    // rotate
-                    if (angle > Math.PI / 3) {
-                        ++states.eva08state.count;
-                        states.eva08state.startEuler.copy(eva08state.endEuler);
-                        states.eva08state.endEuler.setFromQuaternion(
-                            new Quaternion().setFromEuler(
-                                new Euler(
-                                    0,
-                                    (eva08state.count * Math.PI) / 2,
-                                    0
-                                )
-                            )
-                        );
-
-                        set({ v21: eva08state.count }); //
-                        set({ v23: eva08state.startEuler.y }); //
-                        set({ v24: eva08state.endEuler.y }); //
-
-                        states.eva08state.countFlag = false;
-                    }
-                } else {
-                    ref.current.quaternion.rotateTowards(
-                        new Quaternion().setFromEuler(eva08state.startEuler),
-                        config.speed * delta
-                    );
-
-                    set({ v21: ref.current.rotation.y }); //
-                    set({ v22: eva08state.countFlag }); //
-
-                    // check rotate fin
-                    if (
-                        ref.current.rotation.y === eva08state.startEuler.y &&
-                        angle < Math.PI / 16
-                    ) {
-                        states.eva08state.countFlag = true;
-                    }
-                    if (eva08state.count === 0) {
-                        states.eva08state.countFlag = true;
-                    }
-                }
-            }
-        } else {
-            ref.current.quaternion.rotateTowards(
-                new Quaternion().setFromEuler(eva08state.startEuler),
-                config.speed * delta
-            );
-        }
-    });
-
-    return (
-        <group ref={ref}>
-            {/*  */}
-            {children}
-        </group>
-    );
-}
-
-// ==================================================
 function ActionIndex({ children }: EVA08UIProps) {
     const ref = useRef<Group>(new Group());
     const { handResults, eva08state, config } = useSnapshot(states);
     const viewport = useThree((state) => state.viewport);
     const results = new MediapipeHelper(handResults, viewport);
-    const Threshold: number = Math.PI / 16;
 
     const [viewStates, set] = useControls(() => ({
         v0: { value: 0, label: "Finger radian" },
